03bda7d21acf81ae2c65bce10236931054d3e4a9
zhangyc9 <zhangycu@yutong.com>
2021-01-18 16:26:05 +0800

    fdls

---
+++ README.md
@@ -0,0 +1,19 @@
+## 目的
+
+主要是为了学习 vue-router 源码
+
+## 主要实现功能
+
+1. 路由渲染及嵌套路由渲染
+2. this.\$router.push() 方式实现,其他方法可自行扩展
+3. `router-link`及`router-view`实现
+
+## 使用方式
+
+```
+
+1. git clone https://github.com/lyh0371/simple-vue-router.git
+2. npm i
+3. npm run serve
+
+```
---
+++ src/myrouter/create-route-map.js
@@ -0,0 +1,33 @@
+export default function createRouteMap(routes, oldPathList, oldPathMap) {
+  debugger;
+  let pathList = oldPathList || []; // 路径
+  let pathMap = oldPathMap || Object.create(null); // 路由
+
+  routes.forEach((route) => {
+    addRouteRecord(route, pathList, pathMap);
+  });
+
+  return {
+    pathList,
+    pathMap,
+  };
+}
+
+function addRouteRecord(route, pathList, pathMap, parent) {
+  let path = parent ? `${parent.path}/${route.path}` : route.path;
+  let record = {
+    path,
+    component: route.component,
+    parent,
+  };
+  if (!pathMap[path]) {
+    pathList.push(path);
+    pathMap[path] = record;
+  }
+
+  if (route.children) {
+    route.children.forEach((child) => {
+      addRouteRecord(child, pathList, pathMap, record);
+    });
+  }
+}
